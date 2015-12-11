db358179d0dd0269c5fbf7df8b75d1bc654ea4c3
Brandon Martinez <bam2332g@gmail.com>
2015-12-11 18:02:54 +0000

    hopefully last, hardcoded carl

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
 	});
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
